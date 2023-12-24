8a8f4b17e5c9c3db76085a62d6722860ae56415b
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2023-12-25 00:43:31 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dancers4LifeClub from './component/dancerforlife';
 import Header from './component/header';
@@ -13,7 +14,33 @@ import Lander from './component/video';
 import Login from './component/login';
 import Signup from './component/signup';
 import Footer from './component/footer';
-
+const Loader = () => (
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+    <div class="text-center">
+      <div class="spinner-grow text-dark" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  </div>
+  
+  
+  );
+  
+  // Layout component that includes the loader logic
+  const Layout = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      const delayLoader = setTimeout(() => {
+        setIsLoading(false);
+      }, 5000);
+  
+      return () => clearTimeout(delayLoader);
+    }, []);
+  
+    return isLoading ? <Loader /> : children;
+  };
+  
 function App() {
   return (
     
@@ -22,7 +49,7 @@ function App() {
 
     
        <Router>
-       
+           <Layout>
         
      <Header />
  
@@ -35,6 +62,7 @@ function App() {
            <Route path="/sign" element={<Signup />} />
          </Routes>
        {/* <Footer /> */}
+       </Layout>
      </Router>
   );
 }
